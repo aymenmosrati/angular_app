940cc5ecaa5199e9daa0a1b15a38667bb4e5f39e
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-03-22 15:07:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'first_angular_app';
+export class AppComponent implements OnInit {
+  title = 'Welcome to the First Variables';
+  constructor() {}
+  ngOnInit(): void {}
 }
