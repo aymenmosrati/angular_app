52088b9513b5ea4e996f5c322d6ff1ef067829d9
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-03-25 14:50:33 +0100

    ngClass, ngStyle, pipe, uppper lowwer case, currency, json, titlecase, date,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   FirstName = 'Aymen';
   LastName = 'Mosrati';
+  country = 'TUNISIA';
+  titleCladd = 'title';
+
+  profile = {
+    firstName: 'aymen',
+    lastName: 'mosrati',
+    country: 'TUNISIA',
+    titleCladd: 'title',
+  };
+  style1 = 'red';
+  style2 = '30px';
+  style3 = 'underline';
+
+  num = 20;
+  date = '06/18/2021';
 }
